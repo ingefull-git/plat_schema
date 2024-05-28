6e7181cb9d2c7f3ba6549930ac6b6a3787a1859f
rulo_ingefull <rsosa.ingefull@gmail.com>
2024-05-28 20:58:27 +0000

    Found schema differences [ci skip]

--- diffs/platform/schema_diff.sql
+++ diffs/platform/schema_diff.sql
@@ -1,32 +1,13 @@
---- backup/platform/snapshot.sql	2024-05-28 20:56:12.085481350 +0000
-+++ snapshots/platform/snapshot.sql	2024-05-28 20:56:07.000000000 +0000
-@@ -0,0 +1,29 @@
-+--
-+-- PostgreSQL database dump
-+--
-+
+--- backup/platform/snapshot.sql	2024-05-28 20:58:27.419030196 +0000
++++ snapshots/platform/snapshot.sql	2024-05-28 20:58:16.000000000 +0000
+@@ -2,8 +2,8 @@
+ -- PostgreSQL database dump
+ --
+ 
+--- Dumped from database version 16.2 (Debian 16.2-1.pgdg1202)
+--- Dumped by pg_dump version 16.3 (Debian 16.3-1.pgdg1101)
 +-- Dumped from database version 16.2 (Debian 16.2-1.pgdg120+2)
 +-- Dumped by pg_dump version 16.3 (Debian 16.3-1.pgdg110+1)
-+
-+SET statement_timeout = 0;
-+SET lock_timeout = 0;
-+SET idle_in_transaction_session_timeout = 0;
-+SET client_encoding = 'UTF8';
-+SET standard_conforming_strings = on;
-+SELECT pg_catalog.set_config('search_path', '', false);
-+SET check_function_bodies = false;
-+SET xmloption = content;
-+SET client_min_messages = warning;
-+SET row_security = off;
-+
-+--
-+-- Name: platform; Type: SCHEMA; Schema: -; Owner: -
-+--
-+
-+CREATE SCHEMA platform;
-+
-+
-+--
-+-- PostgreSQL database dump complete
-+--
-+
+ 
+ SET statement_timeout = 0;
+ SET lock_timeout = 0;
