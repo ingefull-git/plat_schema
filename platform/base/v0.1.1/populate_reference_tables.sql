54d3be64134f5e6f3c8e69f5a7786ee6434e2356
rulo_ingefull <rsosa.ingefull@gmail.com>
2024-05-30 15:46:14 -0300

    upd populate ref tables

--- platform/base/v0.1.1/populate_reference_tables.sql
+++ platform/base/v0.1.1/populate_reference_tables.sql
@@ -14,35 +14,40 @@ VALUES
     (2, 'FORECAST', 'Forecasted future values'),
     (3, 'ACTUAL_BASELINE', 'Baseline for actual values'),
     (4, 'FORECAST_BASELINE', 'Baseline for forecast values'),
-    (5, 'SIMULATED', 'Simulated or model-generated values');
+    (5, 'SIMULATED', 'Simulated or model-generated values')
+    ON CONFLICT DO NOTHING;
 
 
 INSERT INTO product_type
     (id, name, description)
 VALUES
     (1, 'Energy', 'Energy'),
-    (2, 'Anciliary Service', 'Anciliary Service');
+    (2, 'Anciliary Service', 'Anciliary Service')
+    ON CONFLICT DO NOTHING;
 
 
 INSERT INTO registration_type
     (id, name, description)
 VALUES
     (1, 'asset_group', 'asset_group'),
-    (1, 'asset', 'asset'),
-    (1, 'resource', 'resource');
+    (2, 'asset', 'asset'),
+    (3, 'resource', 'resource')
+    ON CONFLICT DO NOTHING;
 
 
 INSERT INTO resource_type
     (id, name, description)
 VALUES
     (1, 'GEN', 'generation'),
-    (1, 'LOAD', 'load');
+    (2, 'LOAD', 'load')
+    ON CONFLICT DO NOTHING;
 
 
 INSERT INTO asset_data_type
     (id, name, description, schema, asset_type_id)
 VALUES
-    (1, 'strategy', 'strategy', '{"schema":"schema"}', 1);
+    (1, 'strategy', 'strategy', '{"schema":"schema"}', 1)
+    ON CONFLICT DO NOTHING;
 
 
 INSERT INTO asset_meter_type 
@@ -50,28 +55,32 @@ INSERT INTO asset_meter_type
 VALUES
     (1, 'SCADA', 'Supervisory Control and Data Acquisition system integration.'),
     (2, 'BEES', 'Biodiversity and Ecosystem Evaluation System.'),
-    (3, 'third_party', 'Integration with third-party system meters.');
+    (3, 'third_party', 'Integration with third-party system meters.')
+    ON CONFLICT DO NOTHING;
 
 
 INSERT INTO cop_limit_type
     (id, name, description)
 VALUES
     (1, 'HSL', 'HSL'),
-    (1, 'LSL', 'LSL'),
-    (1, 'HEL', 'HEL'),
-    (1, 'LEL', 'LEL');
+    (2, 'LSL', 'LSL'),
+    (3, 'HEL', 'HEL'),
+    (4, 'LEL', 'LEL')
+    ON CONFLICT DO NOTHING;
 
 
 INSERT INTO cop_status_type
     (id, name, description)
 VALUES
-    (1, 'ONTEST', 'ONTEST');
+    (1, 'ONTEST', 'ONTEST')
+    ON CONFLICT DO NOTHING;
  
 
 INSERT INTO market_job_type 
     (id, name, description)
 VALUES
-    (1, 'ISO_PRIVATE_DATA_ETL', 'ISO_PRIVATE_DATA_ETL');
+    (1, 'ISO_PRIVATE_DATA_ETL', 'ISO_PRIVATE_DATA_ETL')
+    ON CONFLICT DO NOTHING;
 
 
 INSERT INTO market_type 
@@ -79,7 +88,8 @@ INSERT INTO market_type
 VALUES
     (1, 'DAM', 'Dayahead Market Type'),
     (2, 'FMM', 'Fifteen-Minute Market Market Type'),
-    (3, 'RTM', 'Realtime Market Type');
+    (3, 'RTM', 'Realtime Market Type')
+    ON CONFLICT DO NOTHING;
 
 
 INSERT INTO offer_type
@@ -87,21 +97,24 @@ INSERT INTO offer_type
 VALUES
     (1, 'BID', 'Buy proposal at specified price.'),
     (2, 'OFFER', 'Sell proposal at specified price.'),
-    (3, 'SCHEDULE', 'Planned energy generation/consumption.');
+    (3, 'SCHEDULE', 'Planned energy generation/consumption.')
+    ON CONFLICT DO NOTHING;
 
 
 INSERT INTO soc_type
     (id, name, description)
 VALUES
     (1, 'initial_soc', 'initial_soc'),
-    (2, 'final_soc', 'final_soc');
+    (2, 'final_soc', 'final_soc')
+    ON CONFLICT DO NOTHING;
 
 
 INSERT INTO task_type
     (id, name, description)
 VALUES
     (1, 'ISO_COP_ETL', 'ETL of the Current Operating Plan'),
-    (2, 'ISO_AWARD_ETL', 'ETL of the Awards');
+    (2, 'ISO_AWARD_ETL', 'ETL of the Awards')
+    ON CONFLICT DO NOTHING;
 
 
 INSERT INTO telemetry_value_type
@@ -115,20 +128,23 @@ VALUES
     (6, 'available discharge (mwh)', 'The current discharging capacity available in megawatt-hours.'),
     (7, 'commanded power (mw)', 'The power output or input commanded to the system in megawatts.'),
     (8, 'system heartbeat', 'A regular signal sent to indicate system status and connectivity.'),
-    (9, 'state of charge', 'The current energy level of the system as a percentage of its total capacity.');
+    (9, 'state of charge', 'The current energy level of the system as a percentage of its total capacity.')
+    ON CONFLICT DO NOTHING;
 
 
 INSERT INTO asset_type
     (id, name, description)
 VALUES
-    (1, 'bess', 'battery energy storage system');
+    (1, 'bess', 'battery energy storage system')
+    ON CONFLICT DO NOTHING;
 
 
 INSERT INTO asset_mode
     (id, name, description)
 VALUES
     (1, 'simulated', 'simulated'),
-    (2, 'live', 'live');
+    (2, 'live', 'live')
+    ON CONFLICT DO NOTHING;
 
 
 
