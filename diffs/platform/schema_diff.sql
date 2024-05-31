0142b9f66f45301f9c933adc12314a2eafc66957
rulo_ingefull <rsosa.ingefull@gmail.com>
2024-05-31 14:03:41 +0000

    Found schema differences [ci error]

--- diffs/platform/schema_diff.sql
+++ diffs/platform/schema_diff.sql
@@ -1,5 +1,5 @@
---- backup/platform/snapshot.sql	2024-05-30 20:41:30.824071993 +0000
-+++ snapshots/platform/snapshot.sql	2024-05-30 20:41:26.000000000 +0000
+--- backup/platform/snapshot.sql	2024-05-31 14:03:41.133547401 +0000
++++ snapshots/platform/snapshot.sql	2024-05-31 14:03:16.000000000 +0000
 @@ -23,6 +23,3234 @@
  CREATE SCHEMA platform;
  
